3a0515c4a3c144c452e3b8ce34727fea98596663
brianleroux <b@brian.io>
2022-07-30 14:35:07 -0700

    tighten up rendering code; passthru location, status, session, etc to html pages

--- src/http/any-catchall/router.mjs
+++ src/http/any-catchall/router.mjs
@@ -13,37 +13,41 @@ import backfill from './_backfill-params.mjs'
 
 export default async function api (basePath, req) {
 
-  // if the route matches api/path/to/thing then we are running this function
   let apiPath = getModule(basePath, 'api', req.rawPath)
   let pagePath = getModule(basePath, 'pages', req.rawPath)
   let state = {}
-
+ 
+  // rendering a json response or passing state to an html response
   if (apiPath) {
+
     // only import if the module exists and only run if export equals httpMethod
     let mod = await import(apiPath)
     let method = mod[req.method.toLowerCase()]
     if (method) {
-
+ 
       // check to see if we need to modify the req and add in params
       req.params = backfill(basePath, apiPath, pagePath, req)
-
+ 
       // grab the state from the app/api route
       state = await method(req)
-
-      // if the user-agent requested json return it immediately
+ 
+      // if the api route does nothing backfill empty json response
+      if (!state) state = { json:{} }
+ 
+      // if the user-agent requested json return the response immediately
       if (isJSON(req.headers)) {
         delete state.location
         return state
       }
-
-      // if not a GET just return api response
-      if (req.method.toLowerCase() != 'get') {
+ 
+      // if not a GET just return api response; or if no corresponding page
+      if (req.method.toLowerCase() != 'get' || !pagePath) {
         return state
       }
     }
   }
-  
-  // always runs next middleware unless we early return
+
+  // rendering an html page
   let { head, elements } = await getElements(basePath)
 
   const html = enhance({
@@ -54,38 +58,37 @@ export default async function api (basePath, req) {
     styleTranstforms: [
       styleTransform
     ],
-    initialState: state.json || {}
+    initialState: state.json? state.json : {}
   })
 
-  if (!pagePath) {
-    const body = html`
-      ${ head({ title: '404' }) }
-      <page-404 error="${req.rawPath} not found"></page-404>
-    `
-    return { status: 404, html: body }
-  }
-
   try {
-    // rendering html page
+
+    // 404
+    if (!pagePath) {
+      const body = html`
+        ${ head({ title: '404' }) }
+        <page-404 error="${req.rawPath} not found"></page-404>
+      `
+      return { status: 404, html: body }
+    }
+
+    // 200
+    let res = {}
     if (pagePath.includes('.html')) {
       let raw = read(pagePath).toString()
-      let body = html`${ head({ title:'' }) }${ raw }`
-      let res = { html: body }
-      if (state.session) 
-        res.session = state.session
-      return res
+      res.html = html`${ head({ title:'' }) }${ raw }`
     }
     else {
-      // rendering custom element page
-      let title = getPageName(basePath, pagePath)
-      let body = html`${ head({ title:'' }) }<page-${ title }></page-${ title }>`
-      let res = { html: body }
-      if (state.session)
-        res.session = state.session
-      return res
+      let tag = getPageName(basePath, pagePath)
+      res.html = html`${ head({ title:'' }) }<page-${ tag }></page-${ tag }>`
     }
+    res.statusCode = state.status || state.code || state.statusCode || 200
+    if (state.session) res.session = state.session
+    if (state.location) res.location = state.location
+    return res
   }
   catch (err) {
+    // 500
     const body = html`
       ${ head({ title: '500' }) }
       <page-500 error="${ err.message }"></page-500>
