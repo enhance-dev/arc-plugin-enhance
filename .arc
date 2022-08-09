89e7d7c884d9eb2d39ec519a4b73d6a3d54f83b7
brianleroux <b@brian.io>
2022-08-08 17:11:51 -0700

    incorporate the new sandbox plugin capabilities; its nice!

--- .arc
+++ .arc
@@ -1,23 +1,9 @@
 @app
 thrasher
 
-@static
-fingerprint true
-
-@http
-
-@views
-src app
-
 @plugins
 architect/plugin-bundles
 beginner
 
 @bundles
 store 'node_modules/@enhance/store'
-
-@tables
-data
-  scopeID *String
-  dataID **String
-  ttl TTL
--- src/plugins/beginner.js
+++ src/plugins/beginner.js
@@ -2,11 +2,35 @@ let path = require('path')
 
 module.exports = { 
   set: {
+
+    static () {
+      return {
+        fingerprint: true
+      }
+    },
+
+    views () {
+      return {
+        src: 'app'
+      }
+    },
+    
     http () {
       let src = path.join(__dirname, '..', 'http', 'any-catchall')
       return [
         { method: 'any', path: '/*', src }
       ]
+    },
+
+    tables () {
+      return {
+        name: 'data',
+        partitionKey: 'scopeID',
+        partitionKeyType: 'string',
+        sortKey: 'dataID',
+        sortKeyType: 'string',
+        ttl: 'TTL'
+      }
     }
-  } 
+  }
 }
