2363ad885abeb743e660f2d59f65c2fc936f852f
brianleroux <b@brian.io>
2022-08-09 19:57:21 -0700

    fix for link in head

--- src/http/any-catchall/templates/head.mjs
+++ src/http/any-catchall/templates/head.mjs
@@ -7,7 +7,7 @@ export default function Head ({ title='' }) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>${title || ''}</title>
-  <link rel="stylesheet" href="arc.static('styles.css')">
+  <link rel="stylesheet" href="${ arc.static('styles.css') }">
 </head>
 `
 }
