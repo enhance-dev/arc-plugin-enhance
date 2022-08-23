61ecc5067d2f54dc9b092bb2360de6aea4c6b400
kj <kj@begin.com>
2022-08-23 14:32:33 -0700

    Removes title so users can set their own

--- src/http/any-catchall/templates/head.mjs
+++ src/http/any-catchall/templates/head.mjs
@@ -1,19 +1,12 @@
 import arc from '@architect/functions'
 
 export default function Head (req, status, error) {
-  const { rawPath } = req
-  const title = status === 200
-    ? rawPath === '/'
-      ? 'Index'
-      : rawPath
-    : status
-
   return `
 <!DOCTYPE html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>${ title }</title>
+  <title></title>
   <link rel="stylesheet" href="${ arc.static('styles.css') }">
   <link rel="icon" href="${ arc.static('favicon.svg') }">
 </head>
