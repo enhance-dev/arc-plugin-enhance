39a47da163e898d2940686d33e5d6fda06078cb6
Ryan Bethel <ryan.bethel@gmail.com>
2022-09-26 11:57:54 -0400

    fixed bug for running sandbox from other directory

--- test/_fingerprint.mjs
+++ test/_fingerprint.mjs
@@ -6,7 +6,7 @@ import fingerprint from '../src/plugins/_fingerprint.js'
 import tiny from 'tiny-json-http'
 import sandbox from '@architect/sandbox'
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 const mockProjectDir = path.join(__dirname,'mocks','app-with-static-assets')
 const mockPublicDir = path.join(mockProjectDir, 'public')
 const mockBuildDir = path.join(mockPublicDir, '_public')
@@ -36,13 +36,41 @@ test('get javascript asset not fingerprinted', async t => {
   
 test('get javascript asset fingerprinted', async t => {
   t.plan(2)
-  const bar = await tiny.get({url:'http://localhost:3333/_public/bar-YYIW2UBG.mjs'})
+  const replacementFile = fs.readFileSync(path.join(mockBuildDir,'replacement-manifest.json'))
+  const replacementManifest = JSON.parse(replacementFile)
+  const bar = await tiny.get({url:`http://localhost:3333/_public/${replacementManifest['bar.mjs']}`})
   t.equal(bar.body, '// test/mocks/app-with-static-assets/public/bar.mjs\n' +
         'var bar_default = "bar";\n' +
         'export {\n' +
         '  bar_default as default\n' +
         '};\n','got fingerprinted mjs')
   t.equal(bar.headers['cache-control'],'max-age=31536000','long cache')
+})
+
+test('Response references transformed', async t => {
+  t.plan(1)
+  const replacementFile = fs.readFileSync(path.join(mockBuildDir,'replacement-manifest.json'))
+  const replacementManifest = JSON.parse(replacementFile)
+  const page = await tiny.get({ url: 'http://localhost:3333/test' })
+  const expected = `<!DOCTYPE html><html lang="en"><head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link rel="stylesheet" href="/_static/styles.css">
+  <link rel="icon" href="/_static/favicon.svg">
+</head>
+<body><img src="/_public/${replacementManifest['favicon.svg']}">
+<script type="module">
+	import bar from '/_public/${replacementManifest['bar.mjs']}'
+</script>
+<script type="module">
+	const foo = await import (\`\${
+'/_public/' + 'foo.mjs'
+}\`)
+</script>
+</body></html>`
+
+  t.equal(page.body, expected, 'page assets fingerprinted')
 })
   
 
