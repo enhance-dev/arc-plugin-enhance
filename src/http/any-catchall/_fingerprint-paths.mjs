17d04622922b8d5b959f7565f9f4202251e316ea
Ryan Bethel <ryan.bethel@gmail.com>
2022-09-27 11:49:58 -0400

    formating white space

--- src/http/any-catchall/_fingerprint-paths.mjs
+++ src/http/any-catchall/_fingerprint-paths.mjs
@@ -5,29 +5,29 @@ import url from 'url'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 let manifest = {}
 try {
-  const manifestFile = fs.readFileSync(path.join(__dirname,'.replacement-manifest.json'))
+  const manifestFile = fs.readFileSync(path.join(__dirname, '.replacement-manifest.json'))
   manifest = JSON.parse(manifestFile)
 } catch (e) {
   console.log('no replacement manifest found')
 }
 
-function replaceAll(str,mapObj){
-    var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
+function replaceAll(str, mapObj) {
+  var re = new RegExp(Object.keys(mapObj).join("|"), "gi");
 
-    return str.replace(re, function(matched){
-        return mapObj[matched.toLowerCase()];
-    });
+  return str.replace(re, function (matched) {
+    return mapObj[matched.toLowerCase()];
+  });
 }
 
 let manifestMap = {}
 const mapEntries = Object.entries(manifest)
 mapEntries.forEach(file => {
-  manifestMap[`_public/${file[0]}`]=`_public/${file[1]}` 
+  manifestMap[`_public/${file[0]}`] = `_public/${file[1]}`
 })
-export default function(str) {
-  if (mapEntries.length===0) {
+export default function (str) {
+  if (mapEntries.length === 0) {
     return str
   } else {
-    return replaceAll(str,manifestMap)
+    return replaceAll(str, manifestMap)
   }
 }
--- src/http/get-_public-catchall/index.mjs
+++ src/http/get-_public-catchall/index.mjs
@@ -5,20 +5,20 @@ import path from 'path'
 import url from 'url'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const staticAliasFile = fs.readFileSync(path.join(__dirname,'.static-manifest.json'))
+const staticAliasFile = fs.readFileSync(path.join(__dirname, '.static-manifest.json'))
 const staticAliases = JSON.parse(staticAliasFile)
 
-export function assetProxy () {
- 
+export function assetProxy() {
+
   const fingerprinted = {
     passthru: true,
     cacheControl: 'max-age=31536000',
   }
-   const notFingerprinted = { 
-    alias:staticAliases,
+  const notFingerprinted = {
+    alias: staticAliases,
     cacheControl: 'max-age=0, must-revalidate',
   }
-  return arc.http.async(asap(fingerprinted),asap(notFingerprinted))
+  return arc.http.async(asap(fingerprinted), asap(notFingerprinted))
 }
 
 export const handler = assetProxy() 
