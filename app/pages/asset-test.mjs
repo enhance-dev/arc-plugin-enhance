ddbfc2c90acfd063e0971a15c3b0927b152b87e0
Ryan Bethel <ryan.bethel@gmail.com>
2022-10-06 13:58:38 -0400

    fixes

---
+++ app/pages/asset-test.mjs
@@ -0,0 +1,9 @@
+import arc from '@architect/functions'
+export default function assets({ html }) {
+return html`<img src="${arc.static('science.png')}" width="50px" height="50px">
+<div>This one uses old school "_static"</div>
+<img src="/_public/science.png" width="50px" height="50px">
+<div>This one uses "_public"</div>
+<img src="/science.png" width="50px" height="50px">
+<div>This one is root relative</div>`
+}
