e864a58a68343cdcb86e04e8f3420ea69a296a44
Ryan Block <rblock@gmail.com>
2022-10-14 11:12:59 -0700

    Fix double base64 encoding issue in `/_public` asset calls

--- src/http/get-_public-catchall/index.mjs
+++ src/http/get-_public-catchall/index.mjs
@@ -1,12 +1,6 @@
-import arc from '@architect/functions'
 import asap from '@architect/asap'
 
-export function assetProxy() {
-
-  function stripPath(req) {
-    req.rawPath = req.rawPath.replace('/_public', '')
-  }
-  return arc.http.async(stripPath, asap())
+export async function handler (req) {
+  req.rawPath = req.rawPath.replace('/_public', '')
+  return asap()(req)
 }
-
-export const handler = assetProxy() 
