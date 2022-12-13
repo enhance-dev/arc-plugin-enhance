0d50ef8e530704f08e1cf2f1fb9170e166b81230
brianleroux <b@brian.io>
2022-12-13 12:37:38 -0800

    adds tests

---
+++ test/mock-async-middleware/app/api/index.mjs
@@ -0,0 +1,13 @@
+export let get = [one, two]
+
+async function one (req) {
+  req.stuff = [1, true, 'yas']
+}
+
+async function two (req) {
+  let stuff = req.stuff
+  return {
+    session: { works: true },
+    json: { stuff }
+  }
+}
