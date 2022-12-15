42c95f1907729247f85480b77d65915ae02cffe2
brianleroux <b@brian.io>
2022-12-15 11:09:19 -0800

    more explicit content negotiation

--- src/http/any-catchall/_is-json-request.mjs
+++ src/http/any-catchall/_is-json-request.mjs
@@ -3,7 +3,8 @@ export default function requestedJSON (headers) {
   let accept = headers['accept'] || headers['Accept']
   let ctype = headers['content-type'] || headers['Content-Type']
   let value = accept || ctype
-  if (value) 
-    return value.includes('/json')
+  if (value) {
+    return value.startsWith('application/json') || value.startsWith('text/json')
+  }
   return false
 }
