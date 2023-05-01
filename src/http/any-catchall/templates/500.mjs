0009dab55c3514eb72fc5a583835a1fc0a39a0f4
kj <kj@begin.com>
2023-05-01 15:59:51 -0700

    Adds minimal styling to 500 page.

--- src/http/any-catchall/templates/500.mjs
+++ src/http/any-catchall/templates/500.mjs
@@ -1,8 +1,18 @@
 export default function FiveHundred ({ html, state }) {
   return html`
-<section>
-  <h1>Oops, something went wrong</h1>
-  <p>${state.attrs.error || 'system failure pleases try again'}</p>
+<style>
+  .max-w {
+    max-width: 30rem;
+  }
+  .min-w {
+    min-width: 18rem;
+  }
+</style>
+<section class="p0 p2-lg">
+  <h1 class="text1 mb2">Oops, something went wrong</h1>
+  <div class="overflow-x-auto">
+    <pre><code>${state.attrs.error || 'system failure please try again'}</code></pre>
+  </div>
 </section>
 `
 }
