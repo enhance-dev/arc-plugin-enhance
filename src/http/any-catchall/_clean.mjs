190617f2bf09895323e6fd6b4224f0811139a15c
Simon MacDonald <simon.macdonald@gmail.com>
2023-07-06 14:01:14 -0400

    Clean Windows

--- src/http/any-catchall/_clean.mjs
+++ src/http/any-catchall/_clean.mjs
@@ -1,4 +1,9 @@
 export default function clean ({ pathTmpl, base, fileNameRegEx }) {
+  if (process.platform === 'win32') {
+    base = base.replace(/\\/g, '/')
+    pathTmpl = pathTmpl.replace(/\\/g, '/')
+  }
+
   return pathTmpl.replace(base, '')
     .replace(fileNameRegEx, '')
     .replace(/(\/?)\$\$\/?$/, '$1(.*)') // $$.mjs is catchall
