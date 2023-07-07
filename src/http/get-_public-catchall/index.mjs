838397dd1e536f082d80fac8af1e0cbba45db839
Ryan Bethel <ryan.bethel@gmail.com>
2023-07-07 11:50:57 -0400

    redirect big ones

--- src/http/get-_public-catchall/index.mjs
+++ src/http/get-_public-catchall/index.mjs
@@ -2,5 +2,15 @@ import asap from '@architect/asap'
 
 export async function handler (req) {
   req.rawPath = req.rawPath.replace('/_public', '')
-  return asap()(req)
+  const response = await asap()(req)
+  console.log('response: ', response)
+  const size = response?.body?.length
+  console.log(`size: ${size}`)
+  if (size > 5500000) {      // 5.5MB
+    return {
+      statusCode: 302,
+      headers: { Location: `/_static${req.rawPath}` }
+    }
+  }
+  return response
 }
