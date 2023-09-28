4f7268e504c5ccf0420060fa0df51baf293a4ecb
brianleroux <b@brian.io>
2023-09-28 15:27:17 -0700

    adds failing test for missing custom header

--- test/router.mjs
+++ test/router.mjs
@@ -32,5 +32,61 @@ test('router with no pages folder', async t => {
   let base = path.join(__dirname, 'mock-folders', 'app')
   let result = await router({ basePath: base }, req)
   t.ok(result, 'got result')
+})
+
+test('headers pass thru application/json', async t => {
+  t.plan(1)
+  let req = {
+    rawPath: '/',
+    method: 'get',
+    headers: {
+      'accept': 'application/json'
+    }
+  }
+  let base = path.join(__dirname, 'mock-headers', 'app')
+  let result = await router({ basePath: base }, req)
+  t.ok(result.headers['x-custom-header'] === 'custom-header-value', 'x-custom-header: custom-header-value')
+})
+
+test('headers pass thru application/json middleware', async t => {
+  t.plan(1)
+  let req = {
+    rawPath: '/',
+    method: 'post',
+    headers: {
+      'accept': 'application/json'
+    }
+  }
+  let base = path.join(__dirname, 'mock-headers', 'app')
+  let result = await router({ basePath: base }, req)
+  t.ok(result.headers['x-custom-header'] === 'custom-header-value', 'x-custom-header: custom-header-value')
+})
+
+test('headers pass thru text/html', async t => {
+  t.plan(1)
+  let req = {
+    rawPath: '/',
+    method: 'get',
+    headers: {
+      'accept': 'text/html'
+    }
+  }
+  let base = path.join(__dirname, 'mock-headers', 'app')
+  let result = await router({ basePath: base }, req)
+  t.ok(result.headers['x-custom-header'] === 'custom-header-value', 'x-custom-header: custom-header-value')
   console.log(result)
+})
+
+test('headers pass thru text/html middleware', async t => {
+  t.plan(1)
+  let req = {
+    rawPath: '/',
+    method: 'post',
+    headers: {
+      'accept': 'text/html'
+    }
+  }
+  let base = path.join(__dirname, 'mock-headers', 'app')
+  let result = await router({ basePath: base }, req)
+  t.ok(result.headers['x-custom-header'] === 'custom-header-value', 'x-custom-header: custom-header-value')
 })
