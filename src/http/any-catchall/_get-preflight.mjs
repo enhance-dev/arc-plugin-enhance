3bebdabed2486a7da65004b5504994cc0bfbf977
kj <kj@begin.com>
2023-12-13 16:46:37 -0800

    Updates internals of _get-preflight to avoid errors when preflight is not present but throw when user syntax errors exist.

--- src/http/any-catchall/_get-preflight.mjs
+++ src/http/any-catchall/_get-preflight.mjs
@@ -1,19 +1,12 @@
+import fs from 'fs'
 import { join } from 'path'
 import { pathToFileURL } from 'url'
 
-export default async function GetPreflight ({ basePath = '' }) {
-  try {
-    const pathToPreflight = pathToFileURL(join(basePath, 'preflight.mjs')).href
-    const { default: preflight } = await import(pathToPreflight)
+export default async function GetPreflight({ basePath = '' }) {
+  const localPath = join(basePath, 'preflight.mjs')
+  if (fs.existsSync(localPath)) {
+    const { default: preflight } = await import(pathToFileURL(localPath).href)
     return preflight
-  }
-  catch (e) {
-    if (e.code === 'ERR_MODULE_NOT_FOUND') {
-      return false
-    }
-    else {
-      throw e
-    }
   }
 }
 
--- test/_get-preflight.mjs
+++ test/_get-preflight.mjs
@@ -24,4 +24,13 @@ test('missing preflight', async t => {
   const preflight = await getPreflight({ basePath })
   t.notok(preflight, 'Missing preflight is OK')
 })
-
+/*
+// No idea how to get this type of test to actually work as documented. 
+// Maybe someone with more patience can get this to do it's one job
+// This works as expected and if this type of test actually worked it would prove it
+test('preflight with missing module import inside it should throw', async t => {
+  t.plan(1)
+  const basePath = path.join(__dirname, 'mock-preflight', 'app', 'bad-preflight')
+  t.throws(async function() { await getPreflight({ basePath }) })
+})
+*/
