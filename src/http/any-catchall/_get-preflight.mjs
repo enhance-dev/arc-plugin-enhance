e32646e7370650a366a441405c8f00eda1705cfd
Taylor Beseda <tbeseda@gmail.com>
2023-12-30 20:36:00 -0700

    try catch preflight import

--- src/http/any-catchall/_get-preflight.mjs
+++ src/http/any-catchall/_get-preflight.mjs
@@ -2,11 +2,16 @@ import fs from 'fs'
 import { join } from 'path'
 import { pathToFileURL } from 'url'
 
-export default async function GetPreflight({ basePath = '' }) {
+export default async function GetPreflight ({ basePath = '' }) {
   const localPath = join(basePath, 'preflight.mjs')
   if (fs.existsSync(localPath)) {
-    const { default: preflight } = await import(pathToFileURL(localPath).href)
-    return preflight
+    try {
+      const { default: preflight } = await import(pathToFileURL(localPath).href)
+      return preflight
+    }
+    catch (error) {
+      throw new Error(`Issue when trying to import preflight: ${localPath}`, { cause: error })
+    }
   }
 }
 
