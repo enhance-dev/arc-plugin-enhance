cede87454c07222690299b601e2d2371b75da405
Taylor Beseda <tbeseda@gmail.com>
2024-03-01 17:37:09 -0700

    simple handler implementation with loader + core

---
+++ src/http/any-catchall/_get-user-file.mjs
@@ -0,0 +1,18 @@
+import { existsSync } from 'node:fs'
+import { join } from 'node:path'
+import { pathToFileURL } from 'node:url'
+
+export default async function getFile (basePath = '', fileName) {
+  const localPath = join(basePath, fileName)
+
+  if (existsSync(localPath)) {
+    try {
+      const { default: fn } = await import(pathToFileURL(localPath).href)
+      return fn
+    }
+    catch (error) {
+      throw new Error(`Issue when trying to import ${fileName}: ${localPath}`, { cause: error })
+    }
+  }
+}
+
--- src/http/any-catchall/index.mjs
+++ src/http/any-catchall/index.mjs
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import process from 'node:process'
 import url from 'node:url'
 
 import arc from '@architect/functions'
@@ -7,12 +8,14 @@ import core from '@enhance/core'
 import importTransform from '@enhance/import-transform'
 import styleTransform from '@enhance/enhance-style-transform'
 
-import { getState, head, preflight, postflight } from './helpers.mjs'
+import fingerprintPublicRefs from './_fingerprint-paths.mjs'
+import getUserFile from './_get-user-file.mjs'
 import _404 from './templates/404.mjs'
 import _500 from './templates/500.mjs'
 import _head from './templates/head.mjs'
 
-const DEBUG = 1
+const { ENHANCE_DEBUG = 0 } = process.env
+const DEBUG = Number(ENHANCE_DEBUG) || 0
 
 const here = path.dirname(url.fileURLToPath(import.meta.url)) // SOMEDAY: import.meta.dirname
 const basePath = path.join(here, 'node_modules', '@architect', 'views')
@@ -26,23 +29,62 @@ const basePath = path.join(here, 'node_modules', '@architect', 'views')
 //   "Core" routeAndRender
 //   post-route: fingerprint paths + merge headers
 
-let config = await loader({ basePath })
+// top-level cache for warm execution
+let config = await loader({ basePath, debug: DEBUG > 1 })
+const userHead = await getUserFile(basePath, 'head.mjs')
+const userPreflight = await getUserFile(basePath, 'preflight.mjs')
 
 export async function createRouter (base) {
   if (base) config = await loader({ basePath: base })
 
-  const app = core({
+  return core({
     ...config,
     ssrOptions: {
       scriptTransforms: [ importTransform({ lookup: arc.static }) ],
       styleTransforms: [ styleTransform ],
     },
-    state: getState(),
-    head,
+    // state: {},
+    head: userHead || _head,
     debug: DEBUG > 0,
   })
-
-  return app
 }
 
-export const handler = (await createRouter()).routeAndRender
+let app
+async function http (req, ctx) {
+  let state = {}
+  if (userPreflight) state = await userPreflight(req)
+  if (!app) app = await createRouter(basePath)
+
+  let response = {}
+  try {
+    response = await app.routeAndRender(req, { ...state, ctx })
+  }
+  catch (error) {
+    console.error(error)
+
+    if (error.message === '404') {
+      response.status = 404
+      response.html = app.render(
+        '<page-404></page-404>',
+        { req, status: 404, error, state },
+        { 'page-404': _404, ...app.elements },
+      )
+    }
+    else {
+      response.status = 500
+      response.html = app.render(
+        '<page-500></page-500>',
+        { req, status: 500, error, state },
+        { 'page-500': _500, ...app.elements },
+      )
+    }
+  }
+
+  // TODO: merge config.timers into response.headers
+
+  response.html = fingerprintPublicRefs(response.html)
+
+  return response
+}
+
+export const handler = arc.http(http)
